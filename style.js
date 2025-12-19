fbe90b02f044d108112f79a9a83734bf20861197
verification01 <verifyproject11@gmail.com>
2025-12-19 17:14:35 +0600

    Add files via upload

---
+++ style.js
@@ -0,0 +1,18 @@
+function saad(){
+	const id = document.getElementById("input").value;
+	if(id === "5027"){
+		window.location.href = "saadinfo.html";
+	}
+	else if(id === "5028"){
+		window.location.href = "santoinfo.html";
+	}
+	
+	
+	
+	
+	
+	
+	else{
+		alert("শিক্ষার্থী খুজে পাওয়া যায়নি")
+	}
+}
